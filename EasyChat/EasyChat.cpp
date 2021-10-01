a54448afdc841b926926326900edc66383f9150b
q1e123 <robertnicolaesolca@gmail.com>
2021-10-01 12:54:14 +0300

    added main code

--- EasyChat/EasyChat.cpp
+++ EasyChat/EasyChat.cpp
@@ -1,20 +1,40 @@
-// EasyChat.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <string>
+#include <vector>
 
-#include <iostream>
+#include "Client.h"
+#include "Server.h"
+#include "Utils.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	std::vector<std::string> parameter_list(argv + 1, argv + argc);
+	auto server_param_iterator = std::find(parameter_list.begin(), parameter_list.end(), "-server");
+	if(server_param_iterator != parameter_list.end())
+	{
+		auto server_name_iterator = std::next(server_param_iterator, 1);
+		auto server_port_it = std::next(server_param_iterator, 2);
+
+		std::string server_name = *server_name_iterator;
+		std::string server_port_string = *server_port_it;
+		size_t server_port = Utils::string_to_size_t(server_port_string);
+
+		std::unique_ptr<Server> server(new Server(server_name, server_port));
+		server->start();
+	}
+	auto client_param_iterator = std::find(parameter_list.begin(), parameter_list.end(), "-client");
+	if (client_param_iterator != parameter_list.end())
+	{
+		auto username_iterator = std::next(client_param_iterator, 1);
+		auto server_ip_iterator = std::next(client_param_iterator, 2);
+		auto server_port_iterator = std::next(client_param_iterator, 2);
+
+		std::string username = *username_iterator;
+		std::string server_ip = *server_ip_iterator;
+		std::string server_port_string = *server_port_iterator;
+		size_t server_port = Utils::string_to_size_t(server_port_string);
+
+		std::unique_ptr<Client> client(new Client(server_port, server_ip, username));
+		client->connect_and_auth();
+		client->start_reciver();
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
