c4263f0a3bb23c9556e97f054c8993d50733e7a4
q1e123 <robertnicolaesolca@gmail.com>
2021-10-01 12:33:00 +0300

    added str to sizet

--- EasyChat/Utils.cpp
+++ EasyChat/Utils.cpp
@@ -1,9 +1,19 @@
 #include "Utils.h"
 
 #include <iostream>
+#include <sstream>
 
 void Utils::memory_error()
 {
 	std::cerr << "MEMORY ERROR: server connection" << std::endl;
 	exit(EXIT_FAILURE);
+}
+
+size_t Utils::string_to_size_t(std::string string)
+{
+
+	std::stringstream sstream(string);
+	size_t result;
+	sstream >> result;
+	return result;
 }
--- EasyChat/Utils.h
+++ EasyChat/Utils.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <string>
+
 namespace Utils
 {
 	void memory_error();
+
+	size_t string_to_size_t(std::string string);
 }
