f34ed5cbc818c28c17d872765caf72094840ed6f
q1e123 <robertnicolaesolca@gmail.com>
2021-10-08 14:59:52 +0300

    added db interface

--- EasyChat/Database_Manager.cpp
+++ EasyChat/Database_Manager.cpp
@@ -1,1 +1,11 @@
 #include "Database_Manager.h"
+std::shared_ptr<DB_Driver> Database_Manager::get_db_driver(Database_Driver_Type database_driver_type)
+{
+	switch (database_driver_type)
+	{
+	case Database_Driver_Type::INI:
+		{
+			std::shared_ptr<DB_Driver> driver(new SimpleIni_Manager());
+		}
+	}
+}
--- EasyChat/Database_Manager.h
+++ EasyChat/Database_Manager.h
@@ -1,5 +1,16 @@
 #pragma once
-class Database_Manager
+
+
+#include <memory>
+
+#include "SimpleIni_Manager.h"
+
+enum Database_Driver_Type
 {
+	INI
 };
 
+namespace Database_Manager
+{
+	std::shared_ptr<DB_Driver> get_db_driver(Database_Driver_Type database_driver_type);
+}
