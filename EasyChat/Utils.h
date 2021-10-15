9c416dc10e226d1a4a5970167c64a2b23abf7415
q1e123 <robertnicolaesolca@gmail.com>
2021-10-15 11:26:48 +0300

    added replace

--- EasyChat/Utils.h
+++ EasyChat/Utils.h
@@ -8,4 +8,6 @@ namespace Utils
 	size_t string_to_size_t(std::string string);
 
 	std::string get_query(std::string file);
+
+	void replace(std::string string, std::string substring_to_replace, std::string replacement);
 }
