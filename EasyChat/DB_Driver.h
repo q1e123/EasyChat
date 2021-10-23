2c7a3eeeace49b2c8a3a9de8988f16e49353dbc6
q1e123 <robertnicolaesolca@gmail.com>
2021-10-23 16:28:41 +0300

    added add auth entry to db driver

--- EasyChat/DB_Driver.h
+++ EasyChat/DB_Driver.h
@@ -6,5 +6,7 @@ class DB_Driver
 public:
 	virtual bool check_authentification(std::string username, std::string password_hash) = 0;
 	virtual void add_user(std::string username, std::string password_hash) = 0;
+
+	virtual void add_authentification_entry(std::string username, std::string status) = 0;
 };
 
