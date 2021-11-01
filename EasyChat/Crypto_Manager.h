37d7a9e0cb5c23bc074c8757e5b1e1cd5e8ec3be
q1e123 <robertnicolaesolca@gmail.com>
2021-11-01 08:56:49 +0200

    added RSA constants

--- EasyChat/Crypto_Manager.h
+++ EasyChat/Crypto_Manager.h
@@ -4,7 +4,14 @@
 
 
 namespace Crypto_Manager{
+	const size_t P = 99371;
+	const size_t Q = 93911;
+	const size_t N = P * Q;
+	const size_t PHI = (P - 1) * (Q - 1);
+
 	std::string get_sha3_512_hash(std::string message);
-	std::string bytes_to_hex_string(const std::vector<uint8_t>& bytes); 
+	std::string bytes_to_hex_string(const std::vector<uint8_t>& bytes);
+	std::string rsa_encrypt(std::string message);
+	std::string rsa_decrypt(std::string message);
 };
 
